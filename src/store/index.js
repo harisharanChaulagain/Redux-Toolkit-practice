9c4d12561d9c03aaa6a027ef4629fe32d8501ec3
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-04-24 22:01:10 +0545

    redux-toolkit-counter-app

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,22 @@
-import { createStore } from "redux";
-const reducerFn = (state = { counter: 0 }, action) => {
+const { configureStore,createSlice } = require("@reduxjs/toolkit");
 
-    if(action.type === 'INC')
-    {
-        return {
-            counter: state.counter+1,
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState : {counter:0},
+    reducers: {
+        increment(state, action) {
+            state.counter ++;
+        },
+        decrement(state, action) {
+            state.counter --;
+        },
+        addBy(state, action) { 
+            state.counter += action.payload;
         }
     }
-    if(action.type === 'DEC')
-    {
-        return {
-            counter: state.counter-1,
-        }
-    }
-    if(action.type === 'ADD')
-    {
-        return{
-            counter: state.counter+action.payload,
-        }
-    }
-
-  return state;
-};
-
-const store = createStore(reducerFn);
-
+})
+export const actions = counterSlice.actions;
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
 export default store;
